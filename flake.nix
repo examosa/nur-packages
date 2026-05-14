9e41bad4ca8d774ba9aeb27cb9e2af77c500b865
Jules Amonith <examosa@fastmail.com>
2026-05-14 15:20:40 -0400

    use systems flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
 {
   description = "My personal NUR repository";
 
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    systems.url = "github:nix-systems/default";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    inherit (nixpkgs) lib;
+    forAllSystems = lib.genAttrs (import systems);
+  in {
+    legacyPackages = forAllSystems (system:
+      import ./default.nix {
         pkgs = nixpkgs.legacyPackages.${system};
       });
 
-      packages = forAllSystems (system: lib.filterAttrs (_: lib.isDerivation) self.legacyPackages.${system});
-    };
+    packages = forAllSystems (system: lib.filterAttrs (_: lib.isDerivation) self.legacyPackages.${system});
+  };
 }
