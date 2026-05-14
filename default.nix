d9728aa3740153da7f607553521f374b8f5fd7b1
Jules Amonith <examosa@fastmail.com>
2026-05-14 15:57:36 -0400

    auto-discover packages

--- default.nix
+++ default.nix
@@ -6,10 +6,14 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
+{pkgs ? import <nixpkgs> {}}:
+{
   # The `lib`, `overlays`, `nixosModules`, `homeModules`,
   # `darwinModules` and `flakeModules` names are special
   overlays = import ./overlays; # nixpkgs overlays
-
-  example-package = pkgs.callPackage ./packages/example-package {};
 }
+// (
+  builtins.mapAttrs
+  (pkg: _: pkgs.callPackage ./packages/${pkg} {})
+  (builtins.readDir ./packages)
+)
