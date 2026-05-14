230a85f3e5aab10232d9eb7424676fc7380ee192
Jules Amonith <examosa@fastmail.com>
2026-05-14 15:21:59 -0400

    clean up overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -1,15 +1,8 @@
 # You can use this file as a nixpkgs overlay. This is useful in the
 # case where you don't want to add the whole NUR namespace to your
 # configuration.
-
-self: super:
-let
-  isReserved = n: n == "lib" || n == "overlays" || n == "nixosModules" || n == "homeModules" || n == "darwinModules" || n == "flakeModules";
-  nameValuePair = n: v: { name = n; value = v; };
-  nurAttrs = import ./default.nix { pkgs = super; };
-
+final: prev: let
+  reserved = ["lib" "overlays" "nixosModules" "homeModules" "darwinModules" "flakeModules"];
+  nurAttrs = import ./default.nix {pkgs = prev;};
 in
-builtins.listToAttrs
-  (map (n: nameValuePair n nurAttrs.${n})
-    (builtins.filter (n: !isReserved n)
-      (builtins.attrNames nurAttrs)))
+  builtins.removeAttrs nurAttrs reserved
